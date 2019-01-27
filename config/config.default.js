1f1c85605838aa659012e75e3a5db544b612c4bd
liujiashun818 <1696054400@qq.com>
2019-01-27 15:15:08 +0800

    'url'

--- config/config.default.js
+++ config/config.default.js
@@ -15,12 +15,14 @@ module.exports = appInfo => {
     // 1,启动mongdb 数据库/随便找个目录新建个文件夹/data，命令行进入此路径，执行‘mongod --dbpath=./data’回车
     // 2,连接数据, node 中连接数据库用 ‘egg-mongoose’ ; 按装-配置-启用
     // 客户端的连接地址
-    url: 'mongodb://127.0.0.1/local' 
+    // url: 'mongodb://127.0.0.1/local' // 本地环境
+    url: 'mongodb://39.107.73.145/local' 
   }
   // 暂时先关闭，安全，上线需要解决
   config.security = {
     csrf: false, // 记得打开时 带上cookie？csfToken
-    domainWhiteList: ['http://localhost:3000']
+    // domainWhiteList: ['http://localhost:3000']
+    domainWhiteList: ['http://39.107.73.145:80']
   }
   config.cors = {
     credentials: true
