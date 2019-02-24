a10791900444d9ad3e8877f1be6d2334ffbd7045
liujiashun818 <1696054400@qq.com>
2019-02-24 10:50:09 +0800

    feat:跨域本地开发

--- config/config.default.js
+++ config/config.default.js
@@ -30,7 +30,8 @@ module.exports = appInfo => {
     credentials: true,
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS',
     allowHeaders: ['Content-Type', 'X-Access-Token', 'Accept', 'x-csrf-token'],
-    origin: 'http://39.107.73.145', // 部署线上的时候加的，不知道干嘛的 跨域用的？
+    // origin: 'http://39.107.73.145', // 部署线上的时候加的，不知道干嘛的 跨域用的？
+    origin: '*', // 部署线上的时候加的，不知道干嘛的 跨域用的？
     credentials: true
   }
   config.cluster = {
